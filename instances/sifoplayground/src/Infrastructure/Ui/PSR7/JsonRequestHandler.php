468da12e458984a5e9dd8bde58065718523d1864
kpicaza <kpicaza.sound@gmail.com>
2019-04-11 21:53:58 +0200

    Implement some use cases with PSR-7 Request handlers

---
+++ instances/sifoplayground/src/Infrastructure/Ui/PSR7/JsonRequestHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sifo\Playground\Infrastructure\Ui\PSR7;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use function sprintf;
+use Zend\Diactoros\Response\JsonResponse;
+
+class JsonRequestHandler implements RequestHandlerInterface
+{
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return new JsonResponse([
+            'message' => 'This is a JSON answer',
+            'back_link' => $this->getUrl($request),
+        ]);
+    }
+
+    private function getUrl(ServerRequestInterface $request): string
+    {
+        return sprintf(
+            '%s://%s%s',
+            $request->getUri()->getScheme(),
+            $request->getUri()->getHost(),
+            $request->getUri()->getPath()
+        );
+    }
+}
