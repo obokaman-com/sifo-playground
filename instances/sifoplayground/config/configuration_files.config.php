3789d772a1e33e5b356b57bdb9baba4c9121bf0c
Albert Garcia <albert.garcia@obokaman.com>
2020-11-24 18:41:10 +0100

    Update SIFO version and fix security issue

--- instances/sifoplayground/config/configuration_files.config.php
+++ instances/sifoplayground/config/configuration_files.config.php
@@ -4,4 +4,5 @@ include ROOT_PATH . '/vendor/sifophp/sifo-common-instance/config/configuration_f
 $config['domains'] = 'instances/sifoplayground/config/domains.config.php';
 $config['locale'] = 'instances/sifoplayground/config/locale.config.php';
 $config['router'] = 'instances/sifoplayground/config/router.config.php';
+$config['services/definition'] = 'instances/sifoplayground/config/services/definition.config.php';
 $config['templates'] = 'instances/sifoplayground/config/templates.config.php';
